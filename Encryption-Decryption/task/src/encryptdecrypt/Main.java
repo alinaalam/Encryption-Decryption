9ddca498fb95b29be7e8c0b06c3f2010d8da4281
alinaalam <alinaalamzeb@gmail.com>
2019-08-27 23:12:24 +0200

    completed Stage 5 of Encryption-Decryption

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,10 +1,17 @@
 package encryptdecrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
 
         String mode = "enc";
         String text = "";
+        String readFile = "";
+        String writeFile = "System.out";
         int key = -1;
 
         for (int i = 0; i < args.length; i++) {
@@ -14,12 +21,38 @@ public class Main {
             if (args[i].equals("-data")) {
                 text = args[i+1];
             }
+            if (args[i].equals("-in")) {
+                readFile = args[i+1];
+            }
+            if (args[i].equals("-out")) {
+                writeFile = args[i+1];
+            }
             if (args[i].equals("-key")) {
                 key = Integer.parseInt(args[i+1]);
             }
         }
 
-        System.out.println((mode.equals("enc") ? encrypt(text, key) : decrypt(text, key)));
+        if (!readFile.isEmpty()) {
+            try {
+                text = new String(Files.readAllBytes(Paths.get(readFile)));
+            } catch (IOException e) {
+                System.out.println("Problem in reading file!");
+            }
+        }
+
+        if (!text.isEmpty()) {
+            String result = mode.equals("enc") ? encrypt(text, key) : decrypt(text, key);
+            if (writeFile.contains("System.out")) {
+                System.out.println(result);
+            }
+            else {
+                try (FileWriter fileWriter = new FileWriter(writeFile)) {
+                    fileWriter.write(result);
+                } catch (IOException e) {
+                    System.out.println("Problem in writing to file!");
+                }
+            }
+        }
     }
 
     public static String encrypt(String plainText, int key) {
